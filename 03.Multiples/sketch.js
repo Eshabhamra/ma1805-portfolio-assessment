06ce5b9ea5207d4eb90915daa021450c205b5663
Eshabhamra <eshabhamra@hotmail.com>
2025-11-03 16:34:49 +0000

    Add files via upload

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -1,3 +1,5 @@
+
+
 /* 
 Playing with mouse events: 
 See https://p5js.org/reference/#/p5/mouseClicked 
@@ -5,7 +7,7 @@ See https://p5js.org/reference/#/p5/mouseClicked
 
 let select = 0; // Color of the shape
 let size = 30; // Size of the shape
-let rgba = [20, 20, 20, 20]; 
+let rgba = [255, 182, 193, 40]; 
 
 function setup() {
   noStroke(); 
@@ -15,22 +17,32 @@ function setup() {
 function draw() {
   fill(rgba);
   ellipse(mouseX, mouseY, size, size);
-  if(select!=2){
+  if (select != 2) {
     size = random(10, 200);
   }
 }
 
 function mouseClicked() {
+  console.log('select: ' + select); 
+  const pastelPinks = [
+    [255, 182, 193], // light pink
+    [255, 192, 203], // pink
+    [255, 209, 220], // very light pink
+    [250, 160, 183], // rose pink
+    [255, 175, 190]  // soft blush
+  ];
   
-   console.log('select: '+select); 
-  
-  rgba[select] = random(0,255); // Set r, g, or b 
-  rgba[3] = random(5,50); // Set alpha (transparency) 
-  select++; // Increment select by 1
- 
-  if(select >= 3){ // if select is greater or equal to...
-    console.log('reset select'); 
-    select = 0; 
+  // Pick a random pastel pink
+  let chosen = random(pastelPinks);
+  rgba[0] = chosen[0];
+  rgba[1] = chosen[1];
+  rgba[2] = chosen[2];
+  rgba[3] = random(20, 60); 
+
+  select++;
+  if (select >= 3) {
+    console.log('reset select');
+    select = 0;
   }
-  return false; // mouseClicked expects this
+
 }
