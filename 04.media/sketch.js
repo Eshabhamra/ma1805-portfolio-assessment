3f7eb271e172be672df3c4fbf9ea85452a7efbbd
Eshabhamra <eshabhamra@hotmail.com>
2025-11-03 16:35:07 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,8 +1,29 @@
+let img;
+
+function preload() {
+  img = loadImage('nightsky.jpg');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  // Keep image proportional and visible
+  let scale = min(width / img.width, height / img.height);
+  let w = img.width * scale;
+  let h = img.height * scale;
+
+  let x = (width - w) / 2;
+  let y = (height - h) / 2;
+
+  image(img, x, y, w, h);
+}
+
+// Optional: resize canvas automatically if window changes
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
