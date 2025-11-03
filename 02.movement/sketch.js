8c7193c0f102b2d974e2fc5cac8440dd1713db94
Eshabhamra <eshabhamra@hotmail.com>
2025-11-03 16:34:30 +0000

    Add files via upload

--- 02.movement/sketch.js
+++ 02.movement/sketch.js
@@ -1,56 +1,38 @@
-let person = 55;
-let cold = false;
-let home;
-let kitchen = "heyyyyy "
-console.log(home)
-let x;
-let ljygojt;
-
-function setup() {
-  createCanvas(400, 400);
-let pet = "cat is big"
-x = random(0,600)
-y = random(90,400)
-}
-
-function draw() {
-  //background(220);
-  //circle
-  fill(0,255,0);
-  circle(50,200, person);
-  person= width/2;
-  console.log(person)
-   // move the circle
-  x = x + speed;
-  // the text 
-  textSize(30);
-  fill(255,0,0);
-  text(kitchen, 100,100)
-
-}
+//let circle
 
 // moving pink circle
-
 let x = 50;
-let speed = 3; 
+let speed = 7; 
+
+let x2 = 30;
+let speed2 = 10; 
 
 function setup() {
-  createCanvas(400, 300); 
+  createCanvas(windowWidth, windowHeight); 
 }
 
-{function draw() 
+
+function draw() {
   background(255); 
-  // draw the circle
+
+  // draw the pink circle
   fill(255, 105, 180); // pink
   noStroke();
   ellipse(x, height / 2, 80, 80); 
-
-  // move the circle
-  x = x + speed;
-
+  x = x + speed; // move hte circle
   // bounce back when hitting edges
   if (x > width - 40 || x < 40) {
     speed = -speed;
   }
 
+
+  // draw the purple circle
+  fill(255, 105, 500); // pink
+  noStroke();
+  ellipse(x2, 300, 80, 80); 
+  x2 = x2 + speed2; // move hte circle
+  // bounce back when hitting edges
+  if (x2 > width - 40 || x2 < 40) {
+    speed2 = -speed2;
+  }
 }
